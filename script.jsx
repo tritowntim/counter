9a2fd2838ae9b612a5180c5890443b110ac10759
Tim Dussinger <tritowntim@gmail.com>
2015-07-08 12:00:46 -0400

    Increment counter by setting state

--- script.jsx
+++ script.jsx
@@ -1,7 +1,13 @@
 var Button = React.createClass({
+  getInitialState: function() {
+    return { counter: 0 };
+  },
+  handleClick: function () {
+    this.setState({counter: this.state.counter + 1});
+  },
   render: function() {
     return (
-      <button>go</button>
+      <button onClick={this.handleClick}>{this.state.counter}</button>
     );
   }
 });
