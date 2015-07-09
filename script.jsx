c591fba7c566d2a4e7951a89b116b5518a701c62
Tim Dussinger <tritowntim@gmail.com>
2015-07-08 21:40:25 -0400

    Reusable component

--- script.jsx
+++ script.jsx
@@ -1,7 +1,11 @@
 var Button = React.createClass({
+  handleClickWithAmount: function() {
+    return this.props.handleClick(this.props.amount);
+  },
   render: function() {
     return (
-      <button onClick={this.props.handleClick}>bump</button>
+      // Alternatively use onClick={this.handleClickWithAmount}
+      <button onClick={this.props.handleClick.bind(this, this.props.amount)}>+{this.props.amount}</button>
     );
   }
 });
@@ -21,13 +25,14 @@ var Main = React.createClass({
   getInitialState: function() {
     return { counter: 0, prevTime: 0, currTime: 0 };
   },
-  increment: function() {
-    this.setState({counter: this.state.counter + 1, prevTime: this.state.currTime, currTime: new Date().getTime()});
+  increment: function(amount) {
+    this.setState({counter: this.state.counter + amount, prevTime: this.state.currTime, currTime: new Date().getTime()});
   },
   render: function() {
     return (
       <div>
-        <Button handleClick={this.increment}/>
+        <Button handleClick={this.increment} amount={1}/>
+        <Button handleClick={this.increment} amount={2}/>
         <Result counter={this.state.counter} prevTime={this.state.prevTime} currTime={this.state.currTime}/>
       </div>
     );
