f057b321826283eeab7f16a4fff417f5e643abb9
Tim Dussinger <tritowntim@gmail.com>
2015-07-08 21:30:59 -0400

    Nested components passing state as props

--- script.jsx
+++ script.jsx
@@ -1,15 +1,37 @@
 var Button = React.createClass({
-  getInitialState: function() {
-    return { counter: 0 };
-  },
-  handleClick: function () {
-    this.setState({counter: this.state.counter + 1});
-  },
   render: function() {
     return (
-      <button onClick={this.handleClick}>{this.state.counter}</button>
+      <button onClick={this.props.handleClick}>bump</button>
     );
   }
 });
 
-React.render(<Button />, document.getElementById('root'));
+var Result = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div>{this.props.counter}</div>
+        <p>{this.props.prevTime} &rarr; {this.props.currTime} ... {this.props.currTime - this.props.prevTime} ms</p>
+      </div>
+    );
+  }
+});
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return { counter: 0, prevTime: 0, currTime: 0 };
+  },
+  increment: function() {
+    this.setState({counter: this.state.counter + 1, prevTime: this.state.currTime, currTime: new Date().getTime()});
+  },
+  render: function() {
+    return (
+      <div>
+        <Button handleClick={this.increment}/>
+        <Result counter={this.state.counter} prevTime={this.state.prevTime} currTime={this.state.currTime}/>
+      </div>
+    );
+  }
+});
+
+React.render(<Main />, document.getElementById('root'));
